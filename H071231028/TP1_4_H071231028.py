fff9cefca63af43bcd676981776a43bf0f8a6351
Anugrah <144884493+NUGRAHSTAR@users.noreply.github.com>
2023-09-14 09:27:06 +0800

    Update TP1_4_H071231028.py

--- H071231028/TP1_4_H071231028.py
+++ H071231028/TP1_4_H071231028.py
@@ -1,16 +1,6 @@
-
 karakter = input (" masukkan jenis karakter : ")
- #
-"huruf(kapital)" "A" <= karakter <= "Z"
-"huruf(kecil)" "a" <= karakter <= "z"
-"angka" "0" <= karakter <= "9"
 
 # Menampilkan hasil
 print("Huruf Kapital?: ", "A" <= karakter <= "Z")
 print("Huruf Kecil?: ", "a" <= karakter <= "z")
 print("Angka?: ", "0" <= karakter <= "9")
-
-
-
-
- 
