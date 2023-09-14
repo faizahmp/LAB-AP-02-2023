969314f136b0eb1e180efdfca31759f070fba24c
Anugrah <144884493+NUGRAHSTAR@users.noreply.github.com>
2023-09-14 09:38:55 +0800

    Delete H071231028/TP1_4_H071231028.py

--- H071231028/TP1_4_H071231028.py
+++
@@ -1,6 +0,0 @@
-karakter = input (" masukkan jenis karakter : ")
-
-# Menampilkan hasil
-print("Huruf Kapital?: ", "A" <= karakter <= "Z")
-print("Huruf Kecil?: ", "a" <= karakter <= "z")
-print("Angka?: ", "0" <= karakter <= "9")
